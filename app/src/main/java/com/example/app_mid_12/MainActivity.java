b0dcf391f8d71337bcfd6fd106c983cbda48466b
3A713135 <mille22554@gmail.com>
2021-11-04 15:04:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/app_mid_12/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.app_mid_12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void image_click(View view){
+        ImageView image=findViewById(R.id.imageView);
+        image.setImageResource(R.drawable._2);
+    }
+}
